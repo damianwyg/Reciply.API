b0b268720f4b6a4726d28f4754464424fa1f0dca
Damian Wygonik <damian.wygonik@gmail.com>
2020-05-27 10:50:23 +0200

    Added user to db

---
+++ Reciply.API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Reciply.API.Models;
+
+namespace Reciply.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
