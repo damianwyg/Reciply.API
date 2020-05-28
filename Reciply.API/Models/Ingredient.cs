b9ca5a40eaad62dda12856c1285e6935d90d5bf1
Damian Wygonik <damian.wygonik@gmail.com>
2020-05-28 13:45:39 +0200

    Changed models

--- Reciply.API/Models/Ingredient.cs
+++ Reciply.API/Models/Ingredient.cs
@@ -3,10 +3,12 @@
     public class Ingredient
     {
         public int IngredientId { get; set; }
-        public int RecipeId { get; set; }
 
         public string Name { get; set; }
         public decimal Quantity { get; set; }
         public string Unit { get; set; }
+
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
     }
 }
--- Reciply.API/Models/Recipe.cs
+++ Reciply.API/Models/Recipe.cs
@@ -4,13 +4,15 @@ namespace Reciply.API.Models
 {
     public class Recipe
     {
-        public int Id { get; set; }
-        public int UserId { get; set; }
+        public int RecipeId { get; set; }
 
         public string Name { get; set; }
         public string Preparation { get; set; }
         public bool IsVegetarian { get; set; }
         public bool IsVegan { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
 
         public ICollection<Ingredient> Ingredients { get; set; }
     }
