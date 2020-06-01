373717b3fcfdc86452f5dd7f56ad050287ae5628
Damian Wygonik <damian.wygonik@gmail.com>
2020-06-01 10:42:24 +0200

    Added photo url to recipe

--- Reciply.API/Migrations/20200601084115_AddUrlToRecipe.Designer.cs
+++ Reciply.API/Migrations/20200601084115_AddUrlToRecipe.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Reciply.API.Data;
 
 namespace Reciply.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200601084115_AddUrlToRecipe")]
+    partial class AddUrlToRecipe
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
