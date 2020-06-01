630b398fa2c03bef5d2e0de8e2cdec8e214b1a20
Damian Wygonik <damian.wygonik@gmail.com>
2020-06-01 12:14:22 +0200

    Added dtos

---
+++ Reciply.API/Dtos/UserForDetailsDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Reciply.API.Models;
+
+namespace Reciply.API.Dtos
+{
+    public class UserForDetailsDto
+    {
+        public int UserId { get; set; }
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string DisplayName { get; set; }
+        public string AboutMe { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string AvatarUrl { get; set; }
+
+        public ICollection<Recipe> Recipes { get; set; }
+    }
+}
---
+++ Reciply.API/Dtos/UserForListingDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reciply.API.Dtos
+{
+    public class UserForListingDto
+    {
+        public int UserId { get; set; }
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string DisplayName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
