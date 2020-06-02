a931374ce3d7c467f8b01f77c5280c1d70605253
Damian Wygonik <damian.wygonik@gmail.com>
2020-06-02 16:50:05 +0200

    Minor changes

--- Reciply.API/Data/AutoMapperProfiles.cs
+++ Reciply.API/Data/AutoMapperProfiles.cs
@@ -9,16 +9,19 @@ namespace Reciply.API.Data.Extensions
         public AutoMapperProfiles()
         {
             CreateMap<User, UserForListingDto>()
-                .ForMember(dest => dest.RecipesCount, opt => {
+                .ForMember(dest => dest.RecipesCount, opt =>
+                {
                     opt.MapFrom(src => src.Recipes.Count);
                 });
             CreateMap<User, UserForDetailsDto>();
             CreateMap<Recipe, RecipeForUserListingDto>();
             CreateMap<Recipe, RecipeForDetailsDto>();
             CreateMap<Ingredient, IngredientForDetailsDto>()
-                .ForMember(dest => dest.Name, opt => {
+                .ForMember(dest => dest.Name, opt =>
+                {
                     opt.MapFrom(src => src.IngredientName.Name);
                 });
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
---
+++ Reciply.API/Dtos/UserForUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reciply.API.Dtos
+{
+    public class UserForUpdateDto
+    {
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+        public string AboutMe { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
