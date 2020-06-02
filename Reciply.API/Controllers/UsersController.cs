16eaf3969b137a40cee4b3b55156e8a3148eb5b3
Damian Wygonik <damian.wygonik@gmail.com>
2020-06-02 16:49:43 +0200

    Added update user

--- Reciply.API/Controllers/UsersController.cs
+++ Reciply.API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -38,6 +40,22 @@ namespace Reciply.API.Controllers
             var usersToReturn = _mapper.Map<IEnumerable<UserForListingDto>>(usersFromRepo);
 
             return Ok(usersToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userFromRepo = await _repo.GetUser(id);
+
+            _mapper.Map(userForUpdateDto, userFromRepo);
+
+            if (await _repo.SaveAllChanges())
+                return NoContent();
+
+            throw new Exception($"Updating user {id} failed on save");
         }
     }
 }
